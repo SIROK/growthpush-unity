94202e46644fe47a3ebd8de3de0082a6ac3a6c8d
Ogawa Shigeru <ogawa@sirok.co.jp>
2015-06-26 21:03:05 +0900

    change growthpush envrionment

--- source/Assets/Plugins/iOS/GrowthPush.mm
+++ source/Assets/Plugins/iOS/GrowthPush.mm
@@ -16,19 +16,7 @@ NSString* NSStringFromCharString(const char* charString) {
 }
 
 extern "C" void growthPushSetApplicationId(int applicationID, const char* secret, int environment, bool debug) {
-	GPEnvironment gpEnvironment = GPEnvironmentUnknown;
-    switch (environment) {
-        case GPEnvironmentDevelopment:
-            gpEnvironment = GPEnvironmentDevelopment;
-            break;
-            
-        case GPEnvironmentProduction:
-            gpEnvironment = GPEnvironmentProduction;
-            break;
-        default:
-            break;
-    }
-    [EasyGrowthPush setApplicationId:applicationID secret:NSStringFromCharString(secret) environment:gpEnvironment debug:debug option:EGPOptionNone];
+    [EasyGrowthPush setApplicationId:applicationID secret:NSStringFromCharString(secret) environment:(GPEnvironment)environment debug:debug option:EGPOptionNone];
 }
 
 extern "C" void growthPushTrackEvent(const char* name, const char* value) {
